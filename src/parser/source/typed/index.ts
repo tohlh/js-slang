06b205c2f6aadcdb880df9f548786733ada3ef1b
tohlh <tohliheng@gmail.com>
2025-02-24 21:43:58 +0800

    Initial prototype of "disallow any" feature

--- src/parser/source/typed/index.ts
+++ src/parser/source/typed/index.ts
@@ -67,6 +67,10 @@ export class SourceTypedParser extends SourceParser {
     }
 
     const typedProgram: TypedES.Program = ast.program as TypedES.Program
+    if (context.prelude !== programStr) {
+      // Check for any declaration only if the program is not the prelude
+      checkForAnyDeclaration(typedProgram, context)
+    }
     const typedCheckedProgram: Program = checkForTypeErrors(typedProgram, context)
     transformBabelASTToESTreeCompliantAST(typedCheckedProgram)
 
@@ -76,4 +80,92 @@ export class SourceTypedParser extends SourceParser {
   toString(): string {
     return 'SourceTypedParser'
   }
+}
+
+function checkForAnyDeclaration(program: TypedES.Program, context: Context) {
+  const config = {
+    allowAnyInVariables: false,
+    allowAnyInParameters: false,
+    allowAnyInReturnType: false
+  }
+
+  function pushAnyUsageError(message: string, node: TypedES.Node) {
+    if (node.loc) {
+      context.errors.push(new FatalSyntaxError(node.loc, message))
+    }
+  }
+
+  function isAnyType(node: TypedES.TSTypeAnnotation | undefined) {
+    return node?.typeAnnotation?.type === 'TSAnyKeyword' || node?.typeAnnotation === undefined
+  }
+
+  function checkNode(node: TypedES.Node) {
+    switch (node.type) {
+      case 'VariableDeclaration': {
+        if (!config.allowAnyInVariables) {
+          node.declarations.forEach(decl => {
+            const tsType = (decl as any).id?.typeAnnotation
+            if (isAnyType(tsType)) {
+              pushAnyUsageError('Usage of "any" in variable declaration is not allowed.', node)
+            }
+            if (decl.init) {
+              // check for lambdas
+              checkNode(decl.init)
+            }
+          })
+        }
+        break
+      }
+      case 'FunctionDeclaration': {
+        if (!config.allowAnyInParameters || !config.allowAnyInReturnType) {
+          const func = node as any
+          // Check parameters
+          if (!config.allowAnyInParameters) {
+            func.params?.forEach((param: any) => {
+              if (isAnyType(param.typeAnnotation)) {
+                pushAnyUsageError('Usage of "any" in function parameter is not allowed.', param)
+              }
+            })
+          }
+          checkNode(node.body)
+        }
+        break
+      }
+      case 'ArrowFunctionExpression': {
+        const func = node as any
+        if (!config.allowAnyInParameters) {
+          func.params?.forEach((param: any) => {
+            if (isAnyType(param.typeAnnotation)) {
+              pushAnyUsageError('Usage of "any" in function parameter is not allowed.', param)
+            }
+          })
+        }
+
+        if (!config.allowAnyInReturnType && isAnyType(func.returnType)) {
+          pushAnyUsageError('Usage of "any" in function return type is not allowed.', node)
+        }
+
+        // Check body (handle single-expression arrow functions or block statements)
+        if (func.body && func.body.type !== 'BlockStatement') {
+          checkNode(func.body)
+        } else {
+          checkNode(node.body)
+        }
+        break
+      }
+      case 'ReturnStatement': {
+        if (node.argument) {
+          checkNode(node.argument)
+        }
+        break
+      }
+      case 'BlockStatement':
+        node.body.forEach(checkNode)
+        break
+      default:
+        break
+    }
+  }
+
+  program.body.forEach(checkNode)
 }
